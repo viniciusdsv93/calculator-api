15d355231c26c0aa364a353fb06a97d84e824667
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-09 06:29:55 -0300

    refactor: implement http helpers and specific error classes

--- src/presentation/controllers/calculator.ts
+++ src/presentation/controllers/calculator.ts
@@ -1,14 +1,19 @@
-export class CalculatorController {
-  async handle(httpRequest: any): Promise<any> {
+import { MissingParamError } from "../errors/missingParamError";
+import { badRequest } from "../helpers/http";
+import { Controller } from "../protocols/controller";
+import { HttpRequest, HttpResponse } from "../protocols/http";
+
+export class CalculatorController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['mathExpression']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return await new Promise(resolve => resolve({
-          statusCode: 400,
-          body: new Error()
-        }))
+        return badRequest(new MissingParamError(field))
       }
     }
-    
+    return {
+      statusCode: 200,
+      body: 'ok'
+    }
   }
 }
---
+++ src/presentation/helpers/http.ts
@@ -0,0 +1,23 @@
+import { ServerError } from "../errors/serverError";
+import { HttpResponse } from "../protocols/http";
+
+export const badRequest = (error: Error): HttpResponse => {
+  return {
+    statusCode: 400,
+    body: error
+  }
+}
+
+export const serverError = (error: Error): HttpResponse => {
+  return {
+    statusCode: 500,
+    body: new ServerError(error.stack as string)
+  }
+}
+
+export const ok = (data: any): HttpResponse => {
+  return {
+    statusCode: 200,
+    body: data
+  }
+}

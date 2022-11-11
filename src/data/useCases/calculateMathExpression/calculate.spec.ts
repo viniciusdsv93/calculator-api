9f5ac03e0c423a07a63a4e85a0835d06fb4eaebf
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-11 08:33:53 -0300

    test: ensure CalculateMathExpressionImpl return the correct result according to the math expression provided

--- src/data/useCases/calculateMathExpression/calculate.spec.ts
+++ src/data/useCases/calculateMathExpression/calculate.spec.ts
@@ -1,12 +1,65 @@
 import { CalculateMathExpressionImpl } from "./calculate";
 
+interface SutTypes {
+	sut: CalculateMathExpressionImpl;
+}
+
+const makeSut = (): SutTypes => {
+	const sut = new CalculateMathExpressionImpl();
+	return {
+		sut,
+	};
+};
+
 describe("Calculate Math Expression", () => {
+	test("Should return an error if invalid value is provided", async () => {
+		const { sut } = makeSut();
+		const response = sut.execute("10 + 10 / (2 + b)");
+		await expect(response).rejects.toThrow();
+	});
+
+	test("Should return an error when trying to divide by zero", async () => {
+		const { sut } = makeSut();
+		const response = sut.execute("10 + 10 / 0");
+		await expect(response).rejects.toThrow();
+	});
+
 	test("Should return the correct result according to the math expression provided", async () => {
-		const sut = new CalculateMathExpressionImpl();
-		const response = await sut.execute("5 + 5");
+		const { sut } = makeSut();
+		const response = await sut.execute("10 + 10");
 		expect(response).toEqual({
-			mathExpression: "5 + 5",
-			result: 10,
+			mathExpression: "10 + 10",
+			result: 20,
+			date: new Date().toLocaleString(),
+		});
+	});
+
+	test("Should return the correct result when using parenthesis", async () => {
+		const { sut } = makeSut();
+		const response = await sut.execute("10 + 10 - (2 + 3)");
+		expect(response).toEqual({
+			mathExpression: "10 + 10 - (2 + 3)",
+			result: 15,
+			date: new Date().toLocaleString(),
+		});
+	});
+
+	test("Should return the correct result when using multiplication", async () => {
+		const { sut } = makeSut();
+		const response = await sut.execute("10 + 10 * (2 + 3)");
+		expect(response).toEqual({
+			mathExpression: "10 + 10 * (2 + 3)",
+			result: 60,
+			date: new Date().toLocaleString(),
+		});
+	});
+
+	test("Should return the correct result when using division", async () => {
+		const { sut } = makeSut();
+		const response = await sut.execute("10 + 10 / (2 + 3)");
+		expect(response).toEqual({
+			mathExpression: "10 + 10 / (2 + 3)",
+			result: 12,
 			date: new Date().toLocaleString(),
 		});
 	});

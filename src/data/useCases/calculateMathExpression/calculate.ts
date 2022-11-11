8e3fdf0702bd193336df4a41dcb012b06b6149c6
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-11 08:33:44 -0300

    feat: ensure CalculateMathExpressionImpl return the correct result according to the math expression provided

--- src/data/useCases/calculateMathExpression/calculate.ts
+++ src/data/useCases/calculateMathExpression/calculate.ts
@@ -1,14 +1,19 @@
+import { evaluate } from "mathjs";
 import { MathResult } from "../../../domain/models/mathResult";
 import { CalculateMathExpression } from "../../../domain/useCases/calculateMathExpression";
 
 export class CalculateMathExpressionImpl implements CalculateMathExpression {
 	async execute(mathExpression: string): Promise<MathResult> {
-		return await new Promise((resolve) =>
-			resolve({
-				mathExpression,
-				result: 10,
-				date: new Date().toLocaleString(),
-			})
-		);
+		const result = await evaluate(mathExpression);
+
+		if (result === Infinity) {
+			throw new Error();
+		}
+
+		return {
+			mathExpression,
+			result,
+			date: new Date().toLocaleString(),
+		};
 	}
 }

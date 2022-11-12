7bb28c458174f27890d5cf225445610381a72c5b
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-12 14:12:36 -0300

    refactor: move sut creation to helper factory methods

--- src/presentation/controllers/getAllMathResults.spec.ts
+++ src/presentation/controllers/getAllMathResults.spec.ts
@@ -2,15 +2,32 @@ import { MathResultModel } from "../../domain/models/mathResult";
 import { GetAllMathResults } from "../../domain/useCases/getAllMathResults";
 import { GetAllMathResultsController } from "./getAllMathResults";
 
+const makeGetAllMathResultsImplStub = (): GetAllMathResults => {
+	class GetAllMathResultsImplStub implements GetAllMathResults {
+		async execute(): Promise<MathResultModel[]> {
+			return [];
+		}
+	}
+	return new GetAllMathResultsImplStub();
+};
+
+interface SutTypes {
+	sut: GetAllMathResultsController;
+	getAllMathResultsImplStub: GetAllMathResults;
+}
+
+const makeSut = (): SutTypes => {
+	const getAllMathResultsImplStub = makeGetAllMathResultsImplStub();
+	const sut = new GetAllMathResultsController(getAllMathResultsImplStub);
+	return {
+		sut,
+		getAllMathResultsImplStub,
+	};
+};
+
 describe("Get All Math Results Controller", () => {
 	test("Should call GetAllMathResultsImpl", async () => {
-		class GetAllMathResultsImplStub implements GetAllMathResults {
-			async execute(): Promise<MathResultModel[]> {
-				return [];
-			}
-		}
-		const getAllMathResultsImplStub = new GetAllMathResultsImplStub();
-		const sut = new GetAllMathResultsController(getAllMathResultsImplStub);
+		const { sut, getAllMathResultsImplStub } = makeSut();
 		const implSpy = jest.spyOn(getAllMathResultsImplStub, "execute");
 		await sut.handle();
 		expect(implSpy).toHaveBeenCalled();

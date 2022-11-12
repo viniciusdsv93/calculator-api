03961c1868688a34c95e44ab68c0f676bab57e1b
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-12 11:15:40 -0300

    config: mariadb database and table creation

---
+++ src/infra/db/mariadb/helpers/mariadbHelper.ts
@@ -0,0 +1,22 @@
+import { createPool } from "mariadb";
+
+const pool = createPool({
+	host: "127.0.0.1",
+	port: 3306,
+	user: "root",
+	password: "password",
+	database: "calculator-mariadb",
+	connectionLimit: 5,
+});
+
+pool.getConnection()
+	.then((conn) => {
+		conn.query(
+			"CREATE OR REPLACE TABLE math_results (id INT, mathExpression VARCHAR(255) NOT NULL, result DECIMAL NOT NULL, date VARCHAR(255) NOT NULL)"
+		);
+	})
+	.catch((err) => {
+		console.log("Error when trying to connect: ", err);
+	});
+
+export { pool };
